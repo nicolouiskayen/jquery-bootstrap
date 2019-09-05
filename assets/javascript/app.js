3a254bec4d42ef3f425f3b87442719905d9931b7
Nicolas Louis-Kayen <nicolouiskayen@gmail.com>
2019-09-04 20:10:28 -0700

    some setup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,8 @@
-console.log("hi")
+$(document).ready(function() {
+    var goal = Math.floor(Math.random() * 100) + 1;
+    var crystal1 = Math.floor(Math.random() * 9) + 1;
+    var crystal2 = Math.floor(Math.random() * 9) + 1;
+    var crystal3 = Math.floor(Math.random() * 9) + 1;
+    var crystal4 = Math.floor(Math.random() * 9) + 1;
+
+});
